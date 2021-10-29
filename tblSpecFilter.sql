92e309f9e68ee8cd35d2becdcd234a0cbaa56af6
Kyle Schaffer <kyle.schaffer@gmail.com>
2021-10-29 09:57:18 -0400

    Updated filter relations

--- tblSpecFilter.sql
+++ tblSpecFilter.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[tblSpecFilter]
 (
-	[Property_ID] INT NOT NULL PRIMARY KEY, 
+	[Property_ID] INT NOT NULL , 
     [SubCategory_ID] INT NOT NULL, 
     [Min_value] NVARCHAR(50) NOT NULL, 
     [Max_value] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [FK_tblSpecFilter_tblSubCategory] FOREIGN KEY ([SubCategory_ID]) REFERENCES [tblSubCategory]([SubCategory_ID])
+    CONSTRAINT [FK_tblSpecFilter_tblSubCategory] FOREIGN KEY ([SubCategory_ID]) REFERENCES [tblSubCategory]([SubCategory_ID]),
+    CONSTRAINT [FK_tblSpecFilter_tblProperty] FOREIGN KEY ([Property_ID]) REFERENCES [tblProperty]([Property_ID]), 
+    CONSTRAINT PK_tblrSpecFilter PRIMARY KEY ([Property_ID],[SubCategory_ID])
 )
--- tblTypeFilter.sql
+++ tblTypeFilter.sql
@@ -4,6 +4,7 @@
     [SubCategory_ID] INT NOT NULL, 
     [Type_Name] NVARCHAR(50) NOT NULL, 
     [Type_Options] NVARCHAR(50) NULL, 
-    [Property_ID] INT NULL, 
-    CONSTRAINT [FK_tblTypeFilter_tblSubCategory] FOREIGN KEY ([SubCategory_ID]) REFERENCES [tblSubCategory]([SubCategory_ID])
+    [Property_ID] INT NOT NULL, 
+    CONSTRAINT [FK_tblTypeFilter_tblSubCategory] FOREIGN KEY ([SubCategory_ID]) REFERENCES [tblSubCategory]([SubCategory_ID]),
+    CONSTRAINT [FK_tblTypeFilter_tblProperty] FOREIGN KEY ([Property_ID]) REFERENCES [tblProperty]([Property_ID])
 )
